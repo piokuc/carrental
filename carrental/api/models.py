2d7e157f98211323f3409cb7928bd8ebf43f0539
piokuc <peter@kuchta.co.uk>
2019-02-10 13:47:00 +0100

    Add Car model.

--- carrental/api/models.py
+++ carrental/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    """
+    Represents a car owned by the car rental company.
+    """
+    registration_number = models.CharField(max_length=10, blank=False, unique=True)
+    make = models.CharField(max_length=255, blank=False)
+    model = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        """Returns a string with the car details."""
+        return "[Car {}: {} {}]".format(self.registration_number,
+                                        self.make, self.model)
--- carrental/api/tests.py
+++ carrental/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Car
+
+
+class CarModelTestCase(TestCase):
+    """
+    Test suite for Car model.
+    """
+
+    def setUp(self):
+        self.car = Car(registration_number = '903X252',
+                       make = "renault",
+                       model = "megane")
+
+
+    def test_model_can_create_a_car(self):
+        old_count = Car.objects.count()
+        self.car.save()
+        new_count = Car.objects.count()
+        self.assertEqual(old_count + 1, new_count)
