cc890a916399581f7e207f56dee553b7fd93202d
piokuc <peter@kuchta.co.uk>
2019-02-15 15:33:14 +0100

    Generate PDF contracts for reservations

--- carrental/api/urls.py
+++ carrental/api/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import (url, include)
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import (CarCreateView, CarDetailsView, ReservationCreateView, ReservationDetailsView)
+from .views import (CarCreateView, CarDetailsView, ReservationCreateView, ReservationDetailsView,
+                    pdf_contract_view)
 
 urlpatterns = {
     url(r'^cars/$', CarCreateView.as_view(), name="create"),
     url(r'^cars/(?P<pk>[0-9]+)/$', CarDetailsView.as_view(), name="details"),
     url(r'^reservations/$', ReservationCreateView.as_view(), name="make_reservation"),
     url(r'^reservations/(?P<pk>[0-9]+)/$', ReservationDetailsView.as_view(), name="reservation_details"),
+    url(r'^pdf_contracts/(?P<pk>[0-9]+)/$', pdf_contract_view, name="pdf_contract"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- carrental/api/views.py
+++ carrental/api/views.py
@@ -2,6 +2,11 @@ from rest_framework import generics
 from .serializers import (CarSerializer, ReservationSerializer)
 from .models import (Car, Reservation)
 from rest_framework.permissions import (IsAuthenticated, IsAdminUser)
+from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 
 class CarCreateView(generics.ListCreateAPIView):
@@ -47,3 +52,34 @@ class ReservationDetailsView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ReservationSerializer
     permission_classes = (IsAuthenticated,)
 
+
+def generate_contract(reservation):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    disposition = 'attachment; filename="car_reservation_no_%s.pdf"'.format(reservation.id)
+    response['Content-Disposition'] = disposition 
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 200, "Car reservation no {}.".format(reservation.id))
+    car = reservation.car
+    p.drawString(100, 150, "Car registration number: {}, {} {}.".format(
+                 car.registration_number, car.make, car.model))
+    p.drawString(100, 130, "Reservation from {} to {}.".format(reservation.start, reservation.end))
+    p.drawString(100, 100, "Signature:")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
+
+
+@login_required
+@api_view(['GET'])
+def pdf_contract_view(request, pk):
+    reservation = get_object_or_404(Reservation, pk=pk)
+    response = generate_contract(reservation)
+    return response
